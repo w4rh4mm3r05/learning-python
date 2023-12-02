62cbe49362588eff77e8a5f8f46a6ec51fb19229
w4rh4mm3r.05 <21076883+w4rh4mm3r05@users.noreply.github.com>
2023-12-02 20:35:45 +0800

    Update Life_in_weeks.py

--- Day02/Life_in_weeks.py
+++ Day02/Life_in_weeks.py
@@ -1,1 +1,13 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
 
+age_wks = float(age) * 52
+
+limit_age = 90
+limit_age_wks = float(limit_age) * 52
+
+
+final_age = limit_age_wks - age_wks
+
+print(f"You have {int(final_age)} weeks left.")
